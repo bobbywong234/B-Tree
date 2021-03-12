beaa27e60ff4785563f1f6af115de87cbbb21bc5
bobbywong234 <36208684+bobbywong234@users.noreply.github.com>
2021-03-12 10:16:52 +0800

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Question_5.Models {
+    public class Data {
+        public uint IdCard { get; set; }
+        public string Name { get; set; }
+    }
+}
